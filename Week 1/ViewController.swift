9ca95eda38b43c3f8d95a0e359de7637e7ca4e0d
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-08-26 17:08:01 -0400

    Initial Commit

---
+++ Week 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week 1
+//
+//  Created by Kyle Gil Tan on 8/26/19.
+//  Copyright © 2019 Kyle Gil Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
